9d28ebb8e22741726ef99f7e3b80105b4fd6904b
Jeremy Faden <faden@cottagesystems.com>
2020-10-28 08:02:19 -0500

    experiment with including md
    Experiment with including md files.  See https://blog.readthedocs.com/adding-markdown-support/

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,11 +44,14 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
 
+from recommonmark.parser import CommonMarkParser
+source_parsers = {'.md': CommonMarkParser}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
